b20d93f24d51436713a470d99173ba21b9e38441
Kai Lüke <pothos@riseup.net>
2011-11-22 03:53:04 +0100

    Die PNM-Lib ist benutzbar :)

--- pnm.hs
+++ pnm.hs
@@ -1,5 +1,3 @@
--- @TODO: Wie kann ein IO Handle weitergereich werden?
-
 import System.IO
 import Data.Word
 import qualified Data.ByteString as B
@@ -24,9 +22,6 @@ toPixel [r, g, b] = (r, g, b)
 fromPixel :: RGB -> [Word8]
 fromPixel (r, g, b) = [r, g, b]
 
-farbigesTestBild :: Width -> Height -> RGB -> Region
-farbigesTestBild w h c = ((0,0), (w,h), w, h, replicate h (replicate w c))
-
 getWidth :: Region -> Int
 getWidth (_, _, w, _, _) = w
 getHeight :: Region -> Int
@@ -44,19 +39,6 @@ getPosX ((x, _), _, _, _, _) = x
 getPosY :: Region -> Int
 getPosY ((_, y), _, _, _, _) = y
 
--- im Moment noch einfache funktion, soll später gelöscht werden
---writePicToFile_deprec :: Region -> String -> IO ()
---writePicToFile_deprec r f = do
---                            h <- openFile f ReadWriteMode
---                            hPutStrLn h "P6"
---                            hPutStrLn h ((show (getWidth r)) ++ " " ++ (show (getHeight r)))
---                            hPutStrLn h "# comment"
---                            hPutStrLn h "###########################"
---                            hPutStrLn h "255"
---                            putStrLn (show (regionToArray r))
---                            B.hPut h (regionToByteString r)
---                            hClose h
-
 
 regionToByteString :: Region -> B.ByteString
 regionToByteString (_, _, _, _, rows) = B.pack(concat(map fromPixel (concat (rows)))) -- concatMap?
@@ -64,12 +46,6 @@ regionToByteString (_, _, _, _, rows) = B.pack(concat(map fromPixel (concat (row
 regionToArray :: Region -> [Word8]
 regionToArray (_, _, _, _, rows) = concat(map fromPixel (concat (rows)))
 
-
--- öffne Datei und gib Handle zurück
---openPNM :: String -> IO Handle
---openPNM f = do
---             h <- openFile f ReadWriteMode
---             return h
 
 -- schreibt Header mit genügend Pufferkommentaren für spätere Größenänderung, festes Byteoffset für einfügen von Regionen
 -- Kommentare nur direkt vor dem Bytestream
@@ -83,10 +59,6 @@ writePNMHeader fn (width, height) = do
                                     where
                                     longheader s = if (length s == bINARYoFFSET-5) then s else longheader (s++"#")
 
--- schließe Datei und gibt Erfolgsstatus zurück
---closeFile :: Handle -> IO ()
---closeFile h = do
---              hClose h
 
 -- macht Seek zur richtigen Position und schreibt Binärdaten
 writeRegionToFile :: String -> Region -> IO ()
@@ -94,11 +66,51 @@ writeRegionToFile fn r = do
                          h <- openFile fn ReadWriteMode
                          let offset = toInteger (bINARYoFFSET + (getPosY r)*(getScreenWidth r)*3 + (getPosX r)*3)
                          hSeek h AbsoluteSeek offset
-                         B.hPut h (regionToByteString r)
+                         foreach h r (regionToByteLines r)
                          hClose h
+                         where
+                         foreach h r (x:xs) = do
+                                        B.hPut h x
+                                        hSeek h RelativeSeek (toInteger (distance r))
+                                        if xs /= [] then foreach h r xs else hSeek h RelativeSeek 0
+                         distance r = ((getScreenWidth r)-(getWidth r))*3
+
+regionToByteLines :: Region -> [B.ByteString]
+regionToByteLines (_, _, _, _, rows) = map (\ r -> B.pack(concat(map fromPixel r))) rows
+-- als kompletter Stream B.pack(concat(map fromPixel (concat (rows))))
 
 -- kopiert als PNG
 saveAsPNG :: String -> IO ProcessHandle
 saveAsPNG s = do
               runCommand ("pnmtopng \"" ++ s ++ "\" > \"`dirname \"" ++ s ++ "\"`/`basename \"" ++ s ++ "\" .pnm`.png\"")
               --runCommand ("./pngconvert.sh "++s)
+
+
+farbigesTestBild :: Width -> Height -> RGB -> Region
+farbigesTestBild w h c = ((0,0), (w,h), w, h, replicate h (replicate w c))
+farbigeTestRegion :: Screen -> Posi -> Width -> Height -> RGB -> Region
+farbigeTestRegion s p w h c = (p, s, w, h, replicate h (replicate w c))
+
+testsequenz = do
+ -- Erstellen von vier.png:
+ writePNMHeader "vier.pnm" (100,100)
+ 
+ let b1 = farbigeTestRegion (100,100) (0,0) 50 50 (0,40,40)
+ writeRegionToFile "vier.pnm" b1
+ 
+ let b2 = farbigeTestRegion (100,100) (50,0) 50 50 (255,255,40)
+ writeRegionToFile "vier.pnm" b2
+ 
+ let b3 = farbigeTestRegion (100,100) (0,50) 50 50 (0,255,40)
+ writeRegionToFile "vier.pnm" b3
+ 
+ let b4 = farbigeTestRegion (100,100) (50,50) 50 50 (0,255,240)
+ writeRegionToFile "vier.pnm" b4
+ 
+ saveAsPNG "vier.pnm"
+ -- auch möglich:
+ let b = farbigesTestBild 100 100 (55,55,55)
+ writePNMHeader "graudannbunt.pnm" (getScreen b)
+ let brechtsunten50quadratabmitte = farbigeTestRegion (getScreen b) (floor((fromIntegral(getWidth b))/2), floor((fromIntegral(getHeight b))/2)) 50 50 (0,255,0)
+ writeRegionToFile "graudannbunt.pnm" brechtsunten50quadratabmitte
+ saveAsPNG "graudannbunt.pnm"
